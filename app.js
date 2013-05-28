b2ee4162e51c9ab89c26560a119202557770a773
Hans Kristian Flaatten <hans@starefossen.com>
2013-05-28 19:59:58 +0200

    Snapping prototype

---
+++ app.js
@@ -0,0 +1,135 @@
+(function() {
+  "use strict";
+  jQuery(function($) {        
+    var waypoints = [];
+    
+    var router = window.location.hash.substr(1) || 'localhost';
+    var areaUrl = 'http://www2.turistforeningen.no/admin/ajax/area.php';
+    var topo = L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
+      maxZoom: 16,
+      attribution: '<a href="http://www.statkart.no/">Statens kartverk</a>'
+    });
+    var map = new L.Map('map', {layers: [topo], center: new L.LatLng(61.5, 9), zoom: 12 });
+    
+    var markers = L.geoJson(null, {
+      style: function (feature) {
+    		return {color: 'blue'};
+    	}
+      /* ,onEachFeature: function(feature, layer) {
+        feature.properties.name = $("<div/>").html(feature.properties.name).text();
+        layer.bindPopup(feature.properties.name);
+        return true;
+      } */
+    }).addTo(map);
+    
+    var routes = L.geoJson(null, {
+      style: function (feature) {
+    		return {color: 'green'};
+    	}
+    	/* ,onEachFeature: function(feature, layer) {
+        feature.properties.name = $("<div/>").html(feature.properties.name).text();
+        layer.bindPopup('Navn: <input name="'+feature.properties.id+'" type="text" value="'+feature.properties.name+'" onchange="featureSaveName(this.name, this.value)"><button>Lagre</button>');
+        return true;
+      } */
+    }).addTo(map);
+    
+    /* $.getJSON(areaUrl + '?key=' + key + '&callback=?', function(data) {
+      for (var i = 0; i < data.features.length; i++) {
+        if (data.features[i].properties.edit) {
+          myPolygons.addData(data.features[i]);
+        } else {
+          otherPolygons.addData(data.features[i]);
+        }
+      }
+    }); */
+        
+    var drawControl = new L.Control.Draw({
+      draw: {
+        position  : 'topleft'
+        ,polyline  : null
+        ,circle    : null
+        ,rectangle : null
+        /* ,marker    : null */
+        ,polygon   : null
+      },
+      edit: {
+        featureGroup: markers,
+        edit: {
+          selectedPathOptions: {
+            color: 'red',
+            opacity: 0.8
+          }
+        }
+      }
+    });
+    map.addControl(drawControl);
+    
+    function tryToRoute() {
+      console.log(markers._layers);
+      if (waypoints.length > 1) {
+        var latlngs = '';
+        for (var i = 0; i < waypoints.length; i++) {
+          if (i > 0) { latlngs += ','; }
+          latlngs += waypoints[i][0] + ',' + waypoints[i][1];
+        }
+        var url = 'http://' + router + '/route/?coords=' + latlngs + '&callback=?';
+        $.get(url, function(data) {
+          try {
+            JSON.parse(data)
+            routes.addData(JSON.parse(data));
+          } catch(e) {
+            console.log('Could not parse JSON');
+            routes.clearLayers()
+          }
+        }, 'jsonp');
+      }    
+    };
+    
+    map.on('draw:created', function (e) {
+      console.log('draw:created', e);
+      waypoints.push([e.layer.getLatLng().lng, e.layer.getLatLng().lat])
+      markers.addLayer(e.layer);
+      tryToRoute();
+            
+      /* var geom, name;
+      
+      name = 'Uten navn';
+      geom = latlngsToString(e.layer._latlngs);
+      
+      $.post(areaUrl + '?key=' + key + '&method=post&callback=?', {'name': name, 'geom': geom}, function(data) {
+        return myPolygons.addData(data);
+      },'jsonp'); */
+    });
+    
+    map.on('draw:deleted', function (e) {
+      console.log('draw:deleted', e);
+      
+      /* e.layers.eachLayer(function (layer) {
+        var id;
+        
+        id = layer.feature.properties.id;
+        
+        $.get(areaUrl + '?key=' + key + '&method=delete&id='+id+'&callback=?', function(data) {
+          // console.log(data);
+        },'jsonp');
+      }); */
+    });
+    
+    map.on('draw:edited', function (e) {
+      console.log('draw:edited', e);
+      tryToRoute();
+      
+      /* e.layers.eachLayer(function (layer) {
+        var id, name, geom;
+        
+        id = layer.feature.properties.id;
+        name = layer.feature.properties.name;
+        geom = latlngsToString(layer._latlngs);
+        
+        $.post(areaUrl + '?key=' + key + '&method=post&id='+id+'&callback=?', {'name': name, 'geom': geom}, function(data) {
+          // return myPolygons.addData(data);
+        },'jsonp');
+      }); */
+    });
+  });
+}).call(this);
---
+++ libs/leaflet-draw/leaflet.draw-no.js
@@ -0,0 +1,15 @@
+L.drawLocal.draw.toolbar.polygon = 'Tegn inn område';
+
+L.drawLocal.draw.polygon.error = '<strong>Feil:</strong> områdekanter kan ikke krysses!';
+L.drawLocal.draw.polygon.tooltip.start = 'Trykk for å starte inntegning.';
+L.drawLocal.draw.polygon.tooltip.cont = 'Trykk for å fortsette inntegning.';
+L.drawLocal.draw.polygon.tooltip.end = 'Trykk på det første punktet for å avslutte inntegning.';
+
+L.drawLocal.edit.toolbar.edit.title = 'Endre områder.';
+L.drawLocal.edit.toolbar.edit.save.title = 'Lagre omrdåer.';
+L.drawLocal.edit.toolbar.edit.save.text = 'Lagre';
+L.drawLocal.edit.toolbar.edit.cancel.title = 'Avbryt endring, lagrer ikke endringer.';
+L.drawLocal.edit.toolbar.edit.cancel.text = 'Abryt';
+
+L.drawLocal.edit.toolbar.remove.title = 'Slett område';
+L.drawLocal.edit.toolbar.remove.tooltip = 'Trykk på et område for å slette det.';
