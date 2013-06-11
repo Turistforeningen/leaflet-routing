ab0872ca1de1b0024a2e7de7e52c9316b2996825
Hans Kristian Flaatten <hans@starefossen.com>
2013-06-11 09:41:03 +0200

    Rewrites module to prototype object

--- app.js
+++ app.js
@@ -5,174 +5,18 @@
 	https://github.com/Turistforeningen/leaflet-routing
 */
 
-var routes;
-
 (function() {
   "use strict";
   jQuery(function($) {        
-    var waypoints = [];
+    var service, topo, map, routing;
     
-    var service = window.location.hash.substr(1) || 'localhost';
-    var bboxUrl = 'http://' + service + '/bbox/?bbox=';
-    var topo = L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
+    service = window.location.hash.substr(1) || 'localhost';
+    topo = L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
       maxZoom: 16,
       attribution: '<a href="http://www.statkart.no/">Statens kartverk</a>'
     });
-    var map = new L.Map('map', {layers: [topo], center: new L.LatLng(61.5, 9), zoom: 13 });
-    
-    // LAYERS
-    var markers = new L.FeatureGroup().addTo(map);
-    routes = new L.FeatureGroup().addTo(map);
-    var snapping = L.geoJson(null, {style:{opacity:0}}).addTo(map);
-
-    // MAP MOVE
-    map.on('moveend', function() {
-      if (map.getZoom() > 12) {
-        var url = bboxUrl + map.getBounds().toBBoxString() + '&callback=?';
-        $.getJSON(url).always(function(data, status) {
-          if (status === 'success') {
-            data = JSON.parse(data);
-            if (typeof data.geometries !== 'undefined' && data.geometries.length > 0) {
-              snapping.clearLayers();
-              snapping.addData(data);
-            }
-          } else {
-            console.error('Could not load snapping data');
-          }          
-        });
-      } else {
-        snapping.clearLayers();
-      }
-    });
-    map.fire('moveend');
-    
-    // SNAPPING OPTIONS
-    var snappingOpts = {
-			enabled       : true
-			,layers       : [snapping]
-			,sensitivity  : 20
-			,vertexonly   : false
-		};
-     
-    // DRAW CONTROL
-    var drawControl = new L.Control.Draw({
-      draw: {
-        position  : 'topleft'
-        ,polyline  : null
-        ,circle    : null
-        ,rectangle : null
-        ,marker    : { snapping: snappingOpts }
-        ,polygon   : null
-      },
-      edit: {
-        featureGroup: markers
-      }
-    })
-    map.addControl(drawControl);
-    
-    function routeDistance(l1, l2, cb) {
-        var latlngs = l1.lng + ',' + l1.lat + ',' + l2.lng + ',' + l2.lat;
-        var url = 'http://' + service + '/route/?coords=' + latlngs + '&callback=?';
-        var req = $.getJSON(url);
-        
-        req.always(function(data, status) {
-          if (status === 'success') {
-            try {
-              L.GeoJSON.geometryToLayer(JSON.parse(data)).eachLayer(function (layer) {
-                return cb(null, layer);
-              });
-            } catch(e) {
-              return cb(true, L.GeoJSON.geometryToLayer({
-                "type": "LineString",
-                "coordinates": [[l1.lng, l1.lat], [l2.lng, l2.lat]]
-              }));
-            }
-          } else {
-            alert('Routing service failed!');
-            return cb(true, L.GeoJSON.geometryToLayer({
-              "type": "LineString",
-              "coordinates": [[l1.lng, l1.lat], [l2.lng, l2.lat]]
-            }));
-          }          
-        });        
-    }
-    
-    function route( curr ) {
-      var prev = curr.routing.prevMarker;
-      var next = curr.routing.nextMarker;
-      
-      // PREV LINE SEGMENT
-      if (prev !== null) {
-        routeDistance(prev.getLatLng(), curr.getLatLng(), function(error, layer) {
-          if (curr.routing.prevLine !== null) {
-            routes.removeLayer(curr.routing.prevLine);
-          }
-          routes.addLayer(layer);
-          curr.routing.prevLine = layer;
-          prev.routing.nextLine = layer;
-        });
-      }
-      
-      // NEXT LINE SEGMENT
-      if (next !== null) {
-        var timeout = (prev !== null ? 1500 : 0);
-        setTimeout(function() {
-          routeDistance(curr.getLatLng(), next.getLatLng(), function(error, layer) {
-            if (curr.routing.nextLine !== null) {
-              routes.removeLayer(curr.routing.nextLine);
-            }
-            routes.addLayer(layer);
-            curr.routing.nextLine = layer;
-            next.routing.prevLine = layer;
-          });
-        }, timeout);
-      }
-    }
-
-    var prevMarker = null;
-    
-    // MARKER CREATED
-    map.on('draw:created', function (e) {
-      e.layer.routing = {
-        prevMarker: prevMarker
-        ,nextMarker: null
-        ,prevLine: null
-        ,nextLine: null
-      };
-      prevMarker = e.layer;
-      markers.addLayer(e.layer);
-      e.layer.dragging.enable();
-      if (e.layer.routing.prevMarker !== null) {
-        e.layer.routing.prevMarker.routing.nextMarker = e.layer;
-        route(e.layer);
-      }
-      var timerID = null;
-      e.layer.on('drag', function(e) {
-        this.setLatLng(L.LineUtil.snapToLayers(this.getLatLng(), this._leaflet_id, snappingOpts));	
-        clearTimeout(timerID);
-        timerID = setTimeout(function(marker) {
-          route(marker);
-        }, 1000, this);
-      });
-      e.layer.on('dragstop', function(e) {
-        route(this);
-      });
-      e.layer.on('dragend', function(e) {
-        route(this);
-      });
-    });
-    
-    // MARKER DELETED
-    map.on('draw:deleted', function (e) {});
-    
-    // MARKER EDITED
-    map.on('draw:edited', function (e) {
-      var i = 0;
-      var fn = function(l) { return route(l); }
-      e.layers.eachLayer(function (layer) {
-        setTimeout(fn, (i*3000), layer);
-        i++;
-      });
-    });
+    map = new L.Map('map', {layers: [topo], center: new L.LatLng(61.5, 9), zoom: 13 });
+    routing = new Routing(map, service);
+  
   });
 }).call(this);
---
+++ libs/leaflet-draw/leaflet.draw.route.js
@@ -0,0 +1,326 @@
+/*
+	Routing capability using the Leaflet framework
+	Copyright (c) 2013, Turistforeningen, Hans Kristian Flaatten
+
+	https://github.com/Turistforeningen/leaflet-routing
+*/
+
+function Routing(map, api) {
+  this.map = map;
+  this.api = api;
+  
+  this.apiBbox = 'http://' + this.api + '/bbox/?bbox=';
+  this.apiRout = 'http://' + this.api + '/route/?coords=';
+  
+  this.layer = {
+    waypoints : new L.FeatureGroup().addTo(this.map)
+    ,segments : new L.FeatureGroup().addTo(this.map)
+    ,snapping : new L.geoJson(null, {style:{opacity:0}}).addTo(this.map)
+  }
+  
+  this.prevWaypoint = null;
+    
+  /**
+   * Init routing
+   *
+   * @access private
+   *
+   * @return void
+  */
+  this._init = function() {
+    this.map.on('moveend', this._bboxReload, this);
+    this.map.fire('moveend');
+    
+    this._initDrawing();
+  };
+  
+  /**
+   * Init Leaflet draw
+   *
+   * @access private
+   *
+   * @return void
+  */
+  this._initDrawing = function() {
+    this._snappingOpts = {
+			enabled       : true
+			,layers       : [this.layer.snapping]
+			,sensitivity  : 20
+			,vertexonly   : false
+		};
+     
+    this._drawControl = new L.Control.Draw({
+      draw: {
+        position  : 'topleft'
+        ,polyline  : null
+        ,circle    : null
+        ,rectangle : null
+        ,marker    : { snapping: this._snappingOpts }
+        ,polygon   : null
+      },
+      edit: {
+        featureGroup: this.layer.waypoints
+      }
+    });
+    this.map.addControl(this._drawControl);
+    
+    this.map.on('draw:created', this._markerOnCreated, this);
+    this.map.on('draw:deleted', this._markerOnDeleted, this);
+    this.map.on('draw:edited', this._makererOnEdited, this);
+  }
+  
+  /**
+   * Realod snapping data using bbox api
+   *
+   * @access private
+   *
+   * return void
+  */
+  this._bboxReload = function() {
+    if (this.map.getZoom() > 12) {
+      var url, $this;
+      
+      url = this.apiBbox + this.map.getBounds().toBBoxString() + '&callback=?';
+      $this = this;
+      
+      $.getJSON(url).always(function(data, status) {
+        if (status === 'success') {
+          data = JSON.parse(data);
+          if (typeof data.geometries !== 'undefined' && data.geometries.length > 0) {
+            $this.layer.snapping.clearLayers();
+            $this.layer.snapping.addData(data);
+          }
+        } else {
+          console.error('Could not load snapping data');
+        }          
+      });
+    } else {
+      this.layer.snapping.clearLayers();
+    }
+  }
+  
+  this._markerOnCreated = function( e ) {
+    if (e.layerType == 'marker') {
+      this.addWaypoint(e.layer, this.prevWaypoint, null);
+      this.prevWaypoint = e.layer;
+    } else {
+      console.error('routing._markerOnCreated unsupported type', e.layerType);
+    }
+  };
+  
+  this._markerOnDeleted = function() {
+    console.log('_markerOnDeleted');
+  };
+  
+  /**
+   * Marker 
+  */
+  this._makererOnEdited = function() {
+    /* var i = 0;
+    var fn = function(l) { return route(l); }
+    e.layers.eachLayer(function (layer) {
+      setTimeout(fn, (i*3000), layer);
+      i++;
+    }); */
+  };
+  
+  /**
+   * Called when marker is being dragged
+   *
+   * @access private
+   *
+   * @param <L.Event> e - marker drag event
+   *
+   * @return void
+  */
+  this._markerOnDrag = function(e) {
+    var $this, marker, latlng, id;
+    
+    $this  = this;
+    marker = e.target;
+    latlng = marker.getLatLng();
+    id     = marker._leaflet_id;
+    
+    marker.setLatLng(L.LineUtil.snapToLayers(latlng, id, this._snappingOpts));	
+    
+    clearTimeout(marker.routing.timeoutID);
+    marker.routing.timeoutID = setTimeout(function(m) {
+      $this._routeWaypoint(m);
+    }, 1000, marker);
+  };
+  
+  /**
+   * Add new waypoint to path
+   *
+   * @access public
+   *
+   * @param <L.Marker> marker - new waypoint marker
+   * @param <L.Marker> prev - previous waypoint marker
+   * @param <L.Marker> next - next waypoint marker
+   * @param <Function> cb - callback method
+   *
+   * @return void
+  */
+  this.addWaypoint = function( marker, prev, next, cb ) {
+    var $this = this;
+    
+    marker.routing = {
+      prevMarker  : prev
+      ,nextMarker : next
+      ,prevLine   : null
+      ,nextLine   : null
+      ,timeoutID  : null
+    };
+    
+    if (marker.routing.prevMarker !== null) {
+      marker.routing.prevMarker.routing.nextMarker = marker;
+      marker.routing.prevLine = marker.routing.prevMarker.routing.nextLine;
+      if (marker.routing.prevLine !== null) {
+        marker.routing.prevLine.routing.nextMarker = marker;
+      }
+    }
+    
+    if (marker.routing.nextMarker !== null) {
+      marker.routing.nextMarker.routing.prevMarker = marker;
+      marker.nextLine = marker.routing.nextMarker.routing.prevLine;
+      if (marker.routing.nextLine !== null) {
+        marker.routing.nextLine.routing.prevMarker = marker;
+      }
+    }
+
+    this.layer.waypoints.addLayer(marker);
+    marker.dragging.enable();
+    marker.on('drag', this._markerOnDrag, this);
+    // marker.on('dragend', this._markerOnDragend, this);
+    
+    if (prev === null || next === null) {
+      $this._routeWaypoint(marker, function(err, data) {
+        if (typeof cb === 'function') {
+          cb(err, data);
+        }
+      });
+    } else {
+      if (typeof cb === 'function') {
+        cb(null, true);
+      }
+    }
+  }
+  
+  /**
+   * Route with respect to waypoint
+   *
+   * @access private
+   *
+   * @param <L.Marker> marker - marker to route on
+   * @param <Function> cb - callback function
+   *
+   * @return void
+  */
+  this._routeWaypoint = function(marker, cb) {
+    
+    if (marker === null) {
+      return cb(null, true);
+    }
+    
+    var $this = this;
+    
+    $this._routeSegment(marker.routing.prevMarker, marker, function(err, data) {
+      setTimeout(function() {
+        $this._routeSegment(marker, marker.routing.nextMarker, function(err, data) {
+          if (typeof cb === 'function') {
+            return cb(null, true);
+          }
+        });      
+      }, 1500);
+    });
+  }
+  
+  /**
+   * Route segment between two markers
+   *
+   * @access private
+   *
+   * @param <L.Marker> m1 - first waypoint marker
+   * @param <L.Marker> m2 - second waypoint marker
+   * @param <Function> cb - callback function (<Error> err, <String> data)
+   *
+   * @return void
+  */
+  this._routeSegment = function(m1, m2, cb) {
+    
+    if (m1 === null || m2 === null) {
+      return cb(null, true);
+    }
+    
+    var $this = this;
+    
+    this.getRoute(m1.getLatLng(), m2.getLatLng(), function(error, layer) {
+      layer.routing = {
+        prevMarker: m1
+        ,nextMarker: m2
+      };
+      
+      if (m1.routing.nextLine !== null) {
+        $this.layer.segments.removeLayer(m1.routing.nextLine);
+      }
+      $this.layer.segments.addLayer(layer);
+      
+      layer.on('click', function(e) {
+        var marker, prev, next;
+        
+        marker = L.marker(e.latlng);
+        prev = e.layer.routing.prevMarker;
+        next = e.layer.routing.nextMarker;
+        
+        $this.addWaypoint(marker, prev, next);
+      }, $this);
+      
+      m1.routing.nextLine = layer;
+      m2.routing.prevLine = layer;
+      
+      cb(null, true);
+    });
+  }
+  
+  /**
+   * Get route between two coordinates
+   *
+   * @access public
+   * 
+   * @param <L.Latlng> l1 - first coordinate
+   * @param <L.Latlng> l2 - second coordinate
+   * @parama <Function> cb - callback function (<Error> err, <ILayer> l)
+   *
+   * @return void
+  */
+  this.getRoute = function(l1, l2, cb) {
+    var latlngs, req;
+    
+    latlngs = l1.lng + ',' + l1.lat + ',' + l2.lng + ',' + l2.lat;
+    req = $.getJSON(this.apiRout + latlngs + '&callback=?');
+    
+    req.always(function(data, status) {
+      if (status === 'success') {
+        try {
+          L.GeoJSON.geometryToLayer(JSON.parse(data)).eachLayer(function (layer) {
+            return cb(null, layer);
+          });
+        } catch(e) {
+          return cb(true, L.GeoJSON.geometryToLayer({
+            "type": "LineString",
+            "coordinates": [[l1.lng, l1.lat], [l2.lng, l2.lat]]
+          }));
+        }
+      } else {
+        alert('Routing service failed!');
+        return cb(true, L.GeoJSON.geometryToLayer({
+          "type": "LineString",
+          "coordinates": [[l1.lng, l1.lat], [l2.lng, l2.lat]]
+        }));
+      }          
+    });
+  }
+  
+  this._init();
+  
+}
